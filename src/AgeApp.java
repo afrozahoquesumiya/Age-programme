e24d3c8600a4e07e4a6973a0c9c2a53049419309
AFROZA HOQUE SUMIYA <amk1005774@student.hamk.fi>
2024-10-17 18:29:24 +0300

    Moped Driving Condition added

--- src/AgeApp.java
+++ src/AgeApp.java
@@ -1,8 +1,11 @@
 public class AgeApp {
     public static void main(String[] args) {
-        int age = 70; 
+        int age = 16; 
         if (age > 0 && age < 18) {
             System.out.println("You are underage.");
+            if (age >= 15) {
+                System.out.println("You can drive a moped.");
+            }
         } else if (age >= 65) {
             System.out.println("You are retired.");
         } else {
