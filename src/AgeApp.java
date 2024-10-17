e8fe9d5f55e10dc0ded651bc06729e53d0cee5ef
AFROZA HOQUE SUMIYA <amk1005774@student.hamk.fi>
2024-10-17 18:17:36 +0300

    Added condition for retirement

--- src/AgeApp.java
+++ src/AgeApp.java
@@ -1,8 +1,10 @@
 public class AgeApp {
     public static void main(String[] args) {
-        int age = 20; 
+        int age = 70; 
         if (age > 0 && age < 18) {
             System.out.println("You are underage.");
+        } else if (age >= 65) {
+            System.out.println("You are retired.");
         } else {
             System.out.println("You are an adult.");
         }
